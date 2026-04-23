1ac794d631bf4bc5cf6f38b050a668fc6e971692
Tharaka Saranga <tharakasaranga755@gmail.com>
2026-04-23 23:38:02 +0530

    make database connection

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Finance Tracker API is running"
+  });
+});
+
+module.exports = app;
---
+++ src/server.js
@@ -0,0 +1,13 @@
+const dotenv = require("dotenv");
+const app = require("./app");
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
